4eedcd84b357370bcd128aea6b55505faaa23fa4
SysAdmin <sysadmin@Medis.local>
2019-03-10 09:34:13 +0330

    Add project files.

---
+++ JobYub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobYub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
